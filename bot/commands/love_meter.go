797c59885a84bc3ef3755165a921c7632a56a97b
ahmetberketuncel <tuncel.ahmet.berke@gmail.com>
2020-12-03 12:55:09 +0300

    update love meter #2

--- bot/commands/love_meter.go
+++ bot/commands/love_meter.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 	embed "github.com/clinet/discordgo-embed"
+	"github.com/skarakasoglu/discord-aybush-bot/configuration"
 	"log"
 	"math/rand"
 	"strings"
@@ -19,6 +20,7 @@ type loveMeterCommand struct{
 	session *discordgo.Session
 }
 
+
 func NewLoveMeterCommand(session *discordgo.Session) Command{
 	return &loveMeterCommand{
 		session: session,
@@ -30,6 +32,10 @@ func (cmd *loveMeterCommand) Name() string{
 }
 
 func (cmd *loveMeterCommand) Execute(message *discordgo.Message) (string, error){
+	if message.ChannelID != configuration.Manager.Channels.Aybus{
+		return "", nil
+	}
+
 	if len(message.Mentions) != 1{
 		return fmt.Sprintf("<@%v>, %v", message.Author.ID, cmd.Usage()), nil
 	}
@@ -47,19 +53,26 @@ func (cmd *loveMeterCommand) Execute(message *discordgo.Message) (string, error)
 	heart = strings.Repeat(heart, love_point/10)
 	bheart = strings.Repeat(bheart, (100 - love_point) / 10 + 1 )
 
-	_, err = cmd.session.ChannelMessageSend(message.ChannelID, fmt.Sprintf("%v - %v%v",love_point, heart,bheart))
-	if err != nil {
-		log.Printf("Error sending message Aybus Channel: %v", err)
+	// Love meter text
+	textIndex := int( love_point / 10)
+	text := configuration.Manager.LoveMeter.Texts[textIndex]
+
+	if member.User.ID == "621427811869261826" && message.Author.ID == "763734826422370305" || member.User.ID == "763734826422370305" && message.Author.ID == "621427811869261826" {
+		text = "Sizi anca zaten birbiriniz bu kadar sevebilir..."
+		love_point = int(1000)
+	}
+
+	if member.User.ID == "534162038751232000" && message.Author.ID == "482955098415431680" || member.User.ID == "482955098415431680" && message.Author.ID == "534162038751232000" {
+		text = "BU AŞK ADETA ÖPÜŞMELİ VURUŞMALI AŞK FİLMLERİNDEN ÇIKMIŞ. İNANILMAZ"
+		love_point = int(150)
 	}
 
 	// Creating new embed
 	newEmbed := embed.NewGenericEmbed("","")
 	newEmbed.Title = fmt.Sprintf("%v#%v ile %v#%v Arasındaki Aşk Sonucu", message.Author.Username,message.Author.Discriminator,member.User.Username,member.User.Discriminator)
 	newEmbed.Color = int(0xBE1931)
-
-	// ÖNEMLİ: Fieldlerde sıkıntı oldu anlamadım nedenini. Neyse benim ara  vermem gerekti o yüzden böyle deployladım.
-	newEmbedField := discordgo.MessageEmbedField{Name: fmt.Sprintf("**Aşy Yüzdesi:** %v", love_point)}
-	newEmbed.Fields = []*discordgo.MessageEmbedField{&newEmbedField}
+	heartsField := discordgo.MessageEmbedField{Name: fmt.Sprintf("**Aşk Yüzdesi:** %v", love_point), Value: fmt.Sprintf("%v%v\n\n%v", heart, bheart, text), Inline: false}
+	newEmbed.Fields = []*discordgo.MessageEmbedField{&heartsField}
 
 	_, err = cmd.session.ChannelMessageSendEmbed(message.ChannelID, newEmbed)
 	if err != nil {
